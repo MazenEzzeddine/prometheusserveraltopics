01cd313021e2c28bbfe280bc14f5244e8670bf9c
m.ezzeddine <mazen.ezzeddine@gmail.com>
2022-11-26 16:01:44 +0100

    sampling and refactoring

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -8,15 +8,15 @@ public class Constants {
   static  String topic1ar = "http://prometheus-operated:9090/api/v1/query?" +
             "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic1%22,namespace=%22default%22%7D%5B1m%5D))%20by%20(topic)";
    static String topic1p0 =   "http://prometheus-operated:9090/api/v1/query?" +
-            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic1%22,partition=%220%22,namespace=%22default%22%7D%5B1m%5D))";
+            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic1%22,partition=%220%22,namespace=%22default%22%7D%5B20s%5D))";
    static  String topic1p1 =   "http://prometheus-operated:9090/api/v1/query?" +
-            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic1%22,partition=%221%22,namespace=%22default%22%7D%5B1m%5D))";
+            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic1%22,partition=%221%22,namespace=%22default%22%7D%5B20s%5D))";
     static String topic1p2 =   "http://prometheus-operated:9090/api/v1/query?" +
-            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic1%22,partition=%222%22,namespace=%22default%22%7D%5B1m%5D))";
+            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic1%22,partition=%222%22,namespace=%22default%22%7D%5B20s%5D))";
     static String topic1p3 =   "http://prometheus-operated:9090/api/v1/query?" +
-            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic1%22,partition=%223%22,namespace=%22default%22%7D%5B1m%5D))";
+            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic1%22,partition=%223%22,namespace=%22default%22%7D%5B20s%5D))";
     static String topic1p4 =   "http://prometheus-operated:9090/api/v1/query?" +
-            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic1%22,partition=%224%22,namespace=%22default%22%7D%5B1m%5D))";
+            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic1%22,partition=%224%22,namespace=%22default%22%7D%5B20s%5D))";
 
 
 
@@ -44,15 +44,15 @@ public class Constants {
     static String topic2ar = "http://prometheus-operated:9090/api/v1/query?" +
             "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic2%22,namespace=%22default%22%7D%5B1m%5D))%20by%20(topic)";
     static String topic2p0 =   "http://prometheus-operated:9090/api/v1/query?" +
-            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic2%22,partition=%220%22,namespace=%22default%22%7D%5B1m%5D))";
+            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic2%22,partition=%220%22,namespace=%22default%22%7D%5B20s%5D))";
     static String topic2p1 =   "http://prometheus-operated:9090/api/v1/query?" +
-            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic2%22,partition=%221%22,namespace=%22default%22%7D%5B1m%5D))";
+            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic2%22,partition=%221%22,namespace=%22default%22%7D%5B20s%5D))";
     static String topic2p2 =   "http://prometheus-operated:9090/api/v1/query?" +
-            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic2%22,partition=%222%22,namespace=%22default%22%7D%5B1m%5D))";
+            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic2%22,partition=%222%22,namespace=%22default%22%7D%5B20s%5D))";
     static String topic2p3 =   "http://prometheus-operated:9090/api/v1/query?" +
-            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic2%22,partition=%223%22,namespace=%22default%22%7D%5B1m%5D))";
+            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic2%22,partition=%223%22,namespace=%22default%22%7D%5B20s%5D))";
     static String topic2p4 =   "http://prometheus-operated:9090/api/v1/query?" +
-            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic2%22,partition=%224%22,namespace=%22default%22%7D%5B1m%5D))";
+            "query=sum(rate(kafka_topic_partition_current_offset%7Btopic=%22testtopic2%22,partition=%224%22,namespace=%22default%22%7D%5B20s%5D))";
 
 
 
